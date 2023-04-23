cbe94b5dc4400541fd48e59d5b7f555da4c3de2e
SaintsConnor <SSGConnor@proton.me>
2023-04-23 16:30:30 +0100

    Update anonychat.py

--- anonychat.py
+++ anonychat.py
@@ -1,15 +1,33 @@
 import argparse
+import sys
+
 from client import Client
+from server import Server
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='AnonyChat command line interface.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-s', '--server', help='run as server', action='store_true')
+    group.add_argument('-c', '--client', help='run as client', action='store_true')
+    parser.add_argument('-a', '--address', help='specify server address to connect to')
+    parser.add_argument('-p', '--port', help='specify server port to connect to', type=int)
+    return parser.parse_args()
+
 
 def main():
-    parser = argparse.ArgumentParser(description="AnonyChat CLI client")
-    parser.add_argument("host", type=str, help="the AnonyChat server host")
-    parser.add_argument("port", type=int, help="the AnonyChat server port")
-    parser.add_argument("username", type=str, help="the user's AnonyChat username")
-    args = parser.parse_args()
+    args = parse_args()
 
-    client = Client(args.host, args.port, args.username)
-    client.start()
+    if args.server:
+        server = Server()
+        server.start()
+    elif args.client:
+        if not args.address or not args.port:
+            print('Error: Please specify server address and port to connect to.')
+            sys.exit(1)
+        client = Client(args.address, args.port)
+        client.start()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
