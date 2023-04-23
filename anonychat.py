6156621fe1e3e53ac81c6bdc958fa3e2e3b2443e
SaintsConnor <SSGConnor@proton.me>
2023-04-23 16:17:58 +0100

    Update anonychat.py

--- anonychat.py
+++ anonychat.py
@@ -1,1 +1,15 @@
+import argparse
+from client import Client
 
+def main():
+    parser = argparse.ArgumentParser(description="AnonyChat CLI client")
+    parser.add_argument("host", type=str, help="the AnonyChat server host")
+    parser.add_argument("port", type=int, help="the AnonyChat server port")
+    parser.add_argument("username", type=str, help="the user's AnonyChat username")
+    args = parser.parse_args()
+
+    client = Client(args.host, args.port, args.username)
+    client.start()
+
+if __name__ == "__main__":
+    main()
